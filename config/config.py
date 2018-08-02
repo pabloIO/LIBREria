1b48cada0882cca74fd8dc29ff292a9925b3d914
pabloIO <pabloIO>
2018-08-02 14:38:16 -0400

    config update

--- config/config.py
+++ config/config.py
@@ -10,9 +10,9 @@ env = {
     'SQL_CONF'  : {
         'DB_NAME'  : 'libreria',
         # 'DB_URI'   : 'mysql://root:arr0wf1r3@localhost/valkiria_chatbot_mysql'
-        'DB_URI'   : str.format('sqlite:///{0}', os.path.abspath('database/libreria.db'))
+        'DB_URI'   : str.format('sqlite:///{0}', os.path.abspath('database/libreria.db')) if 'PYTHONANYWHERE_DOMAIN' not in  os.environ else 'sqlite:////home/lalibreria/LIBREria/database/libreria.db'
     },
-    'UPLOADS_DIR': os.path.abspath('static'),
+    'UPLOADS_DIR': os.path.abspath('static') if 'PYTHONANYWHERE_DOMAIN' not in  os.environ else os.path.abspath('LIBREria/static'),
     'ALLOWED_EXTENSIONS_BOOKS': set(['pdf', 'txt', 'doc', 'docx', 'odt', 'epub']),
     'ALLOWED_EXTENSIONS_IMG': set(['png', 'svg', 'gif', 'jpg', 'jpeg']),
     'API_VERSION': '/api/v1',
